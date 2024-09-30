4fd5860235cab0e397cb73a21b3ff160a173a4c9
Kouhei Niiyama <talukou@gmail.com>
2024-09-30 19:35:11 +0900

    2

--- moji_ui_data/js/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  const fontSlider = document.getElementById('font-slider');
-  const variableFont = document.getElementById('variable-font');
-
-  fontSlider.addEventListener('input', function() {
-    variableFont.style.fontWeight = fontSlider.value;
-  });
-}
